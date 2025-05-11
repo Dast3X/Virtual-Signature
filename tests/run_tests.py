be7228b5d1cfc1050aab2c577c745c2fa74ad88c
Dast3X <65315002+Dast3X@users.noreply.github.com>
2025-05-11 21:57:26 +0300

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -10,6 +10,12 @@ from io import StringIO
 from PySide6.QtWidgets import QApplication
 
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+SRC_PATH = os.path.join(PROJECT_ROOT, "src")
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
+
 class TestResults:
     def __init__(self):
         self.results = []
